904fdf65bf975f03ad66e8e21ead98f09486c756
Venkatesh <vickyvishu77@gmail.com>
2026-09-15 17:25:37 +0530

    Use Instagram app ID and secret

--- src/app/dashboard/connect/ConnectPageClient.tsx
+++ src/app/dashboard/connect/ConnectPageClient.tsx
@@ -7,7 +7,7 @@ import type { SocialAccount } from '@/types'
 
 
 function getMetaOAuthURL() {
-  return `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=1473785194770846&redirect_uri=https://postpilot-1ia.pages.dev/api/meta/callback&response_type=code&scope=instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments`
+  return `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=1745129453437379&redirect_uri=https://postpilot-1ia.pages.dev/api/meta/callback&response_type=code&scope=instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments`
 }
 
 export default function ConnectPageClient({ accounts }: { accounts: SocialAccount[] }) {
